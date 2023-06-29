eb2bf20e0011c72a17ac40c830408bc95a10489f
Nitish Soni <nitishsoni890@gmail.com>
2023-06-30 01:41:56 +0530

    Changed goto Link for test added deployed site link

--- tests/ecommerce.spec.js
+++ tests/ecommerce.spec.js
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 
 test.beforeEach(async ({ page }) => {
-  await page.goto("http://localhost:3000");
+  await page.goto("https://vcriate-task.netlify.app");
 });
 test("Product Load Test", async ({ page }) => {
   const filterpane = await page.getByTestId("filter-pane");
